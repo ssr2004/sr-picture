9d3677a20674920c56e3e2bbe6c64f50a9327d03
ssr <songran.2004@gmail.com>
2026-05-07 17:53:01 +0800

    redis 分布式 session存储“ ”

--- yu-picture-backend/src/main/java/com/yupi/yupicturebackend/model/entity/User.java
+++ yu-picture-backend/src/main/java/com/yupi/yupicturebackend/model/entity/User.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
 
@@ -13,7 +14,9 @@ import lombok.Data;
  */
 @TableName(value ="user")
 @Data
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     /**
      * id
      */
